1b2373072382fadfe594468bc1cf47ba8b03a67e
dkakimoto <shinra.dai0419@gmail.com>
2017-12-19 14:58:50 +0900

    フォームの作成．(擬似的)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <title>チームサザン</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyCNwuP-YysGJfpc3tBq46H-674Hr1Vs_sM&sensor=false">//</script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyBKrcan37P6VmQ8B6EEoCBSoiZW7g4ptug">//</script>
     <script tye="text/javascript" src="js/jquery-2.2.4.min.js">//</script>
     <script tye="text/javascript" src="js/jquery_csv.js">//</script>
     <script tye="text/javascript" src="js/encoding.min.js">//</script>
@@ -11,7 +11,11 @@
   </head>
   <body>
     <h1>寄り道支援アプリケーション</h1>
-    <input id="readbutton" type="file" onchange="read_csv(this.files)" />
+    <form>
+      <input type="text" value="和歌山市駅" id="from">
+      <input type="text" value="和歌山アドベンチャーワールド" id="to">
+      <input type="button" value="マーカ表示" id="button">
+    </form>
     <div style="width:500px;height:300px;" id="map_canvas"></div>
     <?php
     echo "こんにちは";
