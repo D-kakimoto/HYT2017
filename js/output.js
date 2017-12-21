fe44f24d8511eae39af230a06fb075cea5d02d20
dkakimoto <shinra.dai0419@gmail.com>
2017-12-21 19:53:41 +0900

    確認用ボタンの追加と大域変数の変更

--- js/output.js
+++ js/output.js
@@ -1,11 +1,12 @@
 //マップのインスタンスを定義
 var map;
-var f_lat;
-var f_lng;
-var t_lat;
-var t_lng;
-var via_lat;
-var via_lng;
+var from,to;
+var from_to_distance;
+var f_lat, f_lng;
+var t_lat,t_lng;
+var via_lat,via_lng;
+var turn;
+
 
 //初期化処理でGoogle Mapsを表示
 $(function(){
@@ -51,7 +52,7 @@ function set_markers(geoCodeResults,address,type) {
     t_lat = lat;
     t_lng = lng;
   }
-  console.log(location_distance(f_lat,f_lng,t_lat,t_lng));
+  from_to_distance = location_distance(f_lat,f_lng,t_lat,t_lng);
   //console.log(address+"の緯度、経度を取得："+lat+"、"+lng);
   var marker = new google.maps.Marker({
     title:address,
@@ -73,16 +74,27 @@ function attachMessage(marker, msg) {
 //出発地と目的地が送信された時の処理
 window.onload=function(){
   var button = document.getElementById("button");
+  var data_check_button = document.getElementById("data_check");
   // ボタンが押された時の処理
   button.onclick = function(){
     // フォームに入力された住所情報を取得
-    var from = document.getElementById("from").value;
-    var to = document.getElementById("to").value;
+    from = document.getElementById("from").value;
+    to = document.getElementById("to").value;
     if(from != "" && to != ""){
       get_latlng(from,"from_set");
       get_latlng(to,"to_set");
+      route(from,to);
     }else{
       alert("未入力の値があります");
     }
+  }
+  data_check_button.onclick = function(){
+    console.log("取得中データ表示");
+    console.log("出発地:"+from);
+    console.log("目的地:"+to);
+    console.log("出発地から目的地の直線距離:"+from_to_distance);
+    console.log("曲がり角の緯度経度:"+turn);
+    console.log("観光客数に関するcsvデータ:"+get_tourist);
+    console.log("RESASから取得した観光資源情報:"+resource);
   }
 }
