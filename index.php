a5db6e0ac6b61aaa7b69133d1a6329ae5422241f
dkakimoto <shinra.dai0419@gmail.com>
2017-12-21 15:49:49 +0900

    なまえかえた

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <script tye="text/javascript" src="js/resource.js">//</script>
     <script tye="text/javascript" src="js/get_tourist_csv.js">//</script>
     <script tye="text/javascript" src="js/distance.js">//</script>
-    <script tye="text/javascript" src="js/tyouda.js">//</script>
+    <script tye="text/javascript" src="js/popular.js">//</script>
   </head>
 
   <body>
